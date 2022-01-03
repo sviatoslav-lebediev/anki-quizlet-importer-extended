694017392b6d7704fc29072f7a63d90606481126
Hai Nguyen <cognation100@gmail.com>
2022-01-03 18:18:03 -0500

    unfinish merge

--- audio_original.py
+++ audio_original.py
@@ -26,105 +26,44 @@
 #               Update 01/31/2018
 #               - get original quality images instead of mobile version
 #
-# Changlog (by kelciour):
 #               Update 09/12/2018
-#               - updated to Anki 2.1
+#               - updated to Anki 2.1 (by kelciour)
 #
 #               Update 04/02/2020
-#               - download a set without API key since it's no longer working
+#               - download a set without API key since it's no longer working (by kelciour)
 #
 #               Update 19/02/2020
-#               - download private or password-protected sets using cookies
+#               - download private or password-protected sets using cookies (by kelciour)
 #
 #               Update 25/02/2020
-#               - make it work again by adding the User-Agent header
+#               - make it work again by adding the User-Agent header (by kelciour)
 #
 #               Update 14/04/2020
-#               - try to get title from HTML a bit differently
+#               - try to get title from HTML a bit differently (by kelciour)
 #
 #               Update 29/04/2020
-#               - suggest to disable VPN if a set is blocked by a captcha
-#
-#               Update 04/05/2020
-#               - remove Flashcards from the name of the deck
-#               - rename and create a new Basic Quizlet note type if some fields doesn't exist
-#
-#               Update 17/05/2020
-#               - use setPageData and assistantModeData as a possible source for flashcards data
-#
-#               Update 22/07/2020
-#               - fix for Anki 2.1.28
-#
-#               Update 30/08/2020
-#               - add Return shortcut
-#
-#               Update 31/08/2020
-#               - add rich text formatting
-#
-#               Update 03/09/2020
-#               - make it working again after Quizlet update
-
-#               Update 04/09/2020
-#               - move the add-on to GitHub
+#               - suggest to disable VPN if a set is blocked by a captcha (by kelciour)
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
 __window = None
 
-import sys, math, time, urllib.parse, json, re, os
+import sys, math, time, urllib.parse, json, re
 from operator import itemgetter
 
 # Anki
 from aqt import mw
 from aqt.qt import *
-from aqt.utils import showText, tooltip
-from anki.utils import checksum
-
-try:
-    from PyQt5.QtNetwork import QNetworkCookieJar
-except:
-    from PyQt6.QtNetwork import QNetworkCookieJar
+from aqt.utils import showText
 
 import requests
 import shutil
-
-from requests.cookies import RequestsCookieJar
 
 requests.packages.urllib3.disable_warnings()
 
 headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36"
 }
-
-headers = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
-}
-
-rich_text_css = """
-:root {
-  --yellow_light_background: #fff4e5;
-  --blue_light_background: #cde7fa;
-  --pink_light_background: #fde8ff;
-}
-
-.nightMode {
-  --yellow_light_background: #8c7620;
-  --blue_light_background: #295f87;
-  --pink_light_background: #7d537f;
-}
-
-.bgY {
-  background-color: var(--yellow_light_background);
-}
-
-.bgB {
-  background-color: var(--blue_light_background);
-}
-
-.bgP {
-  background-color: var(--pink_light_background);
-}
-"""
 
 # add custom model if needed
 def addCustomModel(name, col):
@@ -203,11 +142,6 @@ class QuizletWindow(QWidget):
 
         self.results = None
         self.thread = None
-        self.closed = False
-
-        self.page = ""
-        self.data = ""
-        self.cookies = self.getCookies()
 
         self.initGUI()
 
@@ -225,7 +159,6 @@ class QuizletWindow(QWidget):
         self.label_url = QLabel("Quizlet URL:")
         self.text_url = QLineEdit("",self)
         self.text_url.setMinimumWidth(300)
-
         self.box_name.addWidget(self.label_url)
         self.box_name.addWidget(self.text_url)
 
@@ -266,7 +199,6 @@ class QuizletWindow(QWidget):
         # code (import set) button
         self.box_code = QHBoxLayout()
         self.button_code = QPushButton("Import Deck", self)
-        self.button_code.setShortcut(QKeySequence("Return"))
         self.box_code.addStretch(1)
         self.box_code.addWidget(self.button_code)
         self.button_code.clicked.connect(self.onCode)
@@ -280,124 +212,22 @@ class QuizletWindow(QWidget):
         self.box_upper.addLayout(self.box_right)
 
         # results label
-        self.label_results = QLabel("Example: https://quizlet.com/515858716/japanese-shops-fruit-flash-cards/")
+        self.label_results = QLabel("\r\n<i>Example: https://quizlet.com/150875612/usmle-flash-cards/</i>")
 
         # add all widgets to top layout
         self.box_top.addLayout(self.box_upper)
-        self.box_top.addSpacing(10)
         self.box_top.addWidget(self.label_results)
         self.box_top.addStretch(1)
         self.setLayout(self.box_top)
-
-        # import by copying the page source code from the web browser
-        # as a way to bypass 403 Forbidden
-        QShortcut(QKeySequence("Ctrl+U"), self, activated=self.getPage)
-        self.text_url.installEventFilter(self) # Fix Ctrl+U on Ubuntu
-
-        # QShortcut(QKeySequence("Ctrl+G"), self, activated=self.resolveCaptcha)
 
         # go, baby go!
         self.setMinimumWidth(500)
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         self.setWindowTitle("Improved Quizlet to Anki Importer")
-        self.resize(self.minimumSizeHint())
         self.show()
 
-    def eventFilter(self, obj: QObject, evt: QEvent):
-        if obj is self.text_url and evt.type() == QEvent.ShortcutOverride:
-            if evt.modifiers() & Qt.ControlModifier and evt.key() == Qt.Key_U:
-                return True
-        return False
-
-    def resolveCaptcha(self, url):
-        url = url.strip()
-        if not url:
-            tooltip("Oops! You forgot the Quizlet URL :(")
-            return
-        d = QDialog(self)
-        d.setWindowTitle("Captcha Challenge")
-        d.setMinimumWidth(500)
-        d.setWindowModality(Qt.WindowModal)
-        l = QVBoxLayout()
-        wv = QWebEngineView()
-        p = QWebEngineProfile("cloudflare", wv)
-        p.setHttpUserAgent(headers["User-Agent"])
-        wp = QWebEnginePage(p, wv)
-        wv.setPage(wp)
-        cs = p.cookieStore()
-        self.captcha = None
-        def onCookieAdded(cookie):
-            if self.captcha is None:
-                self.captcha = QNetworkCookieJar()
-            self.captcha.insertCookie(cookie)
-        cs.cookieAdded.connect(onCookieAdded)
-        wv.load(QUrl(url))
-        bb = QDialogButtonBox(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
-        self.data = ""
-        def setCookiesAndData(data):
-            self.data = data
-            self.cookies = RequestsCookieJar()
-            for c in self.captcha.allCookies():
-                rq = requests.cookies.create_cookie(name=str(c.name(), 'utf-8'), value=str(c.value(), 'utf-8'))
-                rq.domain = c.domain()
-                self.cookies.set_cookie(rq)
-            wp.runJavaScript('JSON.stringify(window.Quizlet["dashboardData"])', getDashboardData)
-        def getDashboardData(text):
-            self.dashboard_data = text
-            d.accept()
-        def getData():
-            wp.runJavaScript('window.Quizlet["setPageData"]["title"] = document.title; JSON.stringify(window.Quizlet["setPageData"])', setCookiesAndData)
-        bb.accepted.connect(getData)
-        bb.rejected.connect(d.reject)
-        l.addWidget(wv)
-        l.addWidget(bb)
-        d.setLayout(l)
-        d.exec_()
-
-    def getPage(self):
-        d = QDialog(self)
-        d.setWindowTitle("Improved Quizlet to Anki Importer")
-        d.setMinimumWidth(400)
-        d.setWindowModality(Qt.WindowModal)
-        l = QVBoxLayout()
-        te = QPlainTextEdit()
-        bb = QDialogButtonBox(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
-        self.page = ""
-        def setPage():
-            self.page = te.toPlainText()
-            d.accept()
-            self.onCode()
-        bb.accepted.connect(setPage)
-        l.addWidget(te)
-        l.addWidget(bb)
-        d.setLayout(l)
-        d.exec_()
-
-    def getCookies(self):
-        config = mw.addonManager.getConfig(__name__)
-
-        cookies = {}
-        if config["qlts"]:
-            cookies = { "qlts": config["qlts"] }
-        elif config["cookies"]:
-            from http.cookies import SimpleCookie
-            C = SimpleCookie()
-            C.load(config["cookies"])
-            cookies = { key: morsel.value for key, morsel in C.items() }
-        return cookies
-
-    def downloadPage(self, url, *args, **kwargs):
-        self.page = ''
-        self.data = ''
-        self.dashboard_data = ''
-        try:
-            r = requests.get(url, *args, **kwargs)
-            r.raise_for_status()
-            self.page = r.text
-        except Exception as e:
-            self.resolveCaptcha(url)
-    
     def onCode(self):
+
         # grab url input
         url = self.text_url.text()
 
@@ -417,45 +247,6 @@ class QuizletWindow(QWidget):
             self.label_results.setText("Oops! That's not a Quizlet URL :(")
             return
 
-        self.button_code.setEnabled(False)
-
-        if "/folders/" not in url:
-            self.downloadSet(url)
-        else:
-            self.downloadPage(url, verify=False, headers=headers, cookies=self.cookies, timeout=15)
-
-            if not self.dashboard_data:
-                regex = re.escape('window.Quizlet["dashboardData"] = ')
-                regex += r'(.+?)'
-                regex += re.escape('; QLoad("Quizlet.dashboardData");')
-                m = re.search(regex, self.page)
-                self.dashboard_data = m.group(1).strip()
-
-            results = json.loads(self.dashboard_data)
-            self.data = ''
-            self.dashboard_data = ''
-
-            assert len(results["models"]["folder"]) == 1
-            quzletFolder = results["models"]["folder"][0]
-            for quizletSet in results["models"]["set"]:
-                if self.closed:
-                    return
-                self.downloadSet(quizletSet["_webUrl"], quzletFolder["name"])
-                self.sleep(1.5)
-
-        self.button_code.setEnabled(True)
-
-    def closeEvent(self, evt):
-        self.closed = True
-        evt.accept()
-
-    def sleep(self, seconds):
-        start = time.time()
-        while time.time() - start < seconds:
-            time.sleep(0.01)
-            QApplication.instance().processEvents()
-
-    def downloadSet(self, urlPath, parentDeck=""):
         # validate and set Quizlet deck ID
         quizletDeckID = urlPath.strip("/")
         if quizletDeckID == "":
@@ -471,18 +262,10 @@ class QuizletWindow(QWidget):
         self.label_results.setText("Connecting to Quizlet...")
 
         # build URL
-        # deck_url = ("https://api.quizlet.com/2.0/sets/{0}".format(quizletDeckID))
-        # deck_url += ("?client_id={0}".format(QuizletWindow.__APIKEY))
-        # deck_url = "https://quizlet.com/{}/flashcards".format(quizletDeckID)
-        deck_url = urlPath
-
-        # stop previous thread first
-        # if self.thread is not None:
-        #     self.thread.terminate()
+        deck_url = "https://quizlet.com/{}/flashcards".format(quizletDeckID)
 
         # download the data!
-        self.downloadPage(deck_url, verify=False, headers=headers, cookies=self.cookies, timeout=15)
-        self.thread = QuizletDownloader(self, deck_url, page=self.page, data=self.data)
+        self.thread = QuizletDownloader(self, deck_url)
         self.thread.start()
 
         while not self.thread.isFinished():
@@ -507,22 +290,14 @@ class QuizletWindow(QWidget):
             deck = self.thread.results
             # self.label_results.setText(("Importing deck {0} by {1}...".format(deck["title"], deck["created_by"])))
             self.label_results.setText(("Importing deck {0}...".format(deck["title"])))
-            self.createDeck(deck, parentDeck)
+            self.createDeck(deck)
             # self.label_results.setText(("Success! Imported <b>{0}</b> ({1} cards by <i>{2}</i>)".format(deck["title"], deck["term_count"], deck["created_by"])))
             self.label_results.setText(("Success! Imported <b>{0}</b> ({1} cards)".format(deck["title"], deck["term_count"])))
 
         # self.thread.terminate()
         self.thread = None
-        self.page = ""
-        self.data = ""
 
-    def createDeck(self, result, parentDeck=""):
-        config = mw.addonManager.getConfig(__name__)
-
-        if config["rich_text_formatting"] and not os.path.exists("_quizlet.css"):
-            with open("_quizlet.css", "w") as f:
-                f.write(rich_text_css.lstrip())
-
+    def createDeck(self, result):
         # create new deck and custom model
         if "set" in result:
             name = result['set']['title']
@@ -531,10 +306,7 @@ class QuizletWindow(QWidget):
         else:
             name = result['title']
 
-        if parentDeck:
-            name = "{}::{}".format(parentDeck, name)
-        
-        items = result['items']
+        items= result['items']
         progress = 0
 
         result['term_count'] = len(items)
@@ -601,7 +373,7 @@ class QuizletWindow(QWidget):
         url = url.replace('_m', '')
         file_name = "quizlet-" + suffix if suffix else  "quizlet-" + url.split('/')[-1]
         # get original, non-mobile version of images
-        r = requests.get(url, stream=True, verify=False, headers=headers, timeout=15)
+        r = requests.get(url, stream=True, verify=False, headers=headers)
         if r.status_code == 200:
             with open(file_name, 'wb') as f:
                 r.raw.decode_content = True
@@ -648,10 +420,9 @@ class QuizletDownloader(QThread):
     # thread that downloads results from the Quizlet API
     def __init__(self, window, url):
         super(QuizletDownloader, self).__init__()
-        self.url = url
         self.window = window
-        self.page = page
-        self.data = data
+
+        self.url = url
         self.results = None
 
         self.error = False
@@ -663,52 +434,57 @@ class QuizletDownloader(QThread):
     def run(self):
         r = None
         try:
-            if self.data:
-                self.results = json.loads(self.data)
-            else:
-                text = self.page
+            config = mw.addonManager.getConfig(__name__)
 
-                regex = re.escape('window.Quizlet["setPasswordData"]')
+            cookies = {}
+            if config["qlts"]:
+                cookies = { "qlts": config["qlts"] }
+            elif config["cookies"]:
+                from http.cookies import SimpleCookie
+                C = SimpleCookie()
+                C.load(config["cookies"])
+                cookies = { key: morsel.value for key, morsel in C.items() }
 
-                if re.search(regex, text):
-                    self.error = True
-                    self.errorCode = 403
-                    return
+            r = requests.get(self.url, verify=False, headers=headers, cookies=cookies)
+            r.raise_for_status()
 
-                regex = re.escape('window.Quizlet["setPageData"] = ')
+            regex = re.escape('window.Quizlet["setPasswordData"]')
+
+            if re.search(regex, r.text):
+                self.error = True
+                self.errorCode = 403
+                return
+
+            regex = re.escape('window.Quizlet["setPageData"] = ')
+            regex += r'(.+?)'
+            regex += re.escape('; QLoad("Quizlet.setPageData");')
+            m = re.search(regex, r.text)
+
+            if not m:
+                regex = re.escape('window.Quizlet["assistantModeData"] = ')
                 regex += r'(.+?)'
-                regex += re.escape('; QLoad("Quizlet.setPageData");')
-                m = re.search(regex, text)
+                regex += re.escape('; QLoad("Quizlet.assistantModeData");')
+                m = re.search(regex, r.text)
 
-                if not m:
-                    regex = re.escape('window.Quizlet["assistantModeData"] = ')
-                    regex += r'(.+?)'
-                    regex += re.escape('; QLoad("Quizlet.assistantModeData");')
-                    m = re.search(regex, text)
+            if not m:
+                regex = re.escape('window.Quizlet["cardsModeData"] = ')
+                regex += r'(.+?)'
+                regex += re.escape('; QLoad("Quizlet.cardsModeData");')
+                m = re.search(regex, r.text)
 
-                if not m:
-                    regex = re.escape('window.Quizlet["cardsModeData"] = ')
-                    regex += r'(.+?)'
-                    regex += re.escape('; QLoad("Quizlet.cardsModeData");')
-                    m = re.search(regex, text)
+            data = m.group(1).strip()
+            self.results = {}
+            self.results['items'] = mapItems(json.loads(data))
 
-                assert m, 'NO MATCH\n\n' + text
-
-                self.data = m.group(1).strip()
-
-                title = os.path.basename(self.url.strip()) or "Quizlet Flashcards"
-                m = re.search(r'<title>(.+?)</title>', text)
-                if m:
-                    title = m.group(1)
-                    title = re.sub(r' \| Quizlet$', '', title)
-                    title = re.sub(r'^Flashcards ', '', title)
-                    title = re.sub(r'\s+', ' ', title)
-                    title = title.strip()
-
-                self.results = json.loads(self.data)
-
-                self.results['title'] = title
-
+            title = os.path.basename(self.url.strip()) or "Quizlet Flashcards"
+            m = re.search(r'<title>(.+?)</title>', r.text)
+            if m:
+                title = m.group(1)
+                title = re.sub(r' \| Quizlet$', '', title)
+                title = re.sub(r'^Flashcards ', '', title)
+                title = re.sub(r'\s+', ' ', title)
+                title = title.strip()
+            self.results['title'] = title
         except requests.HTTPError as e:
             self.error = True
             self.errorCode = e.response.status_code
@@ -720,7 +496,7 @@ class QuizletDownloader(QThread):
             self.errorMessage = "Invalid json: {0}".format(e)
         except Exception as e:
             self.error = True
-            self.errorMessage = "{}\n-----------------\n{}".format(e, r.text if r else "")
+            self.errorMessage = "{}\n-----------------\n{}".format(e, r.text)
         # yep, we got it
 
 # plugin was called from Anki
